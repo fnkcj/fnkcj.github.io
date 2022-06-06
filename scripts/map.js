ba7e968f8d4380491ca3e7f3c4fb21c344d5653a
fnkcj <fnkattack@gmail.com>
2022-06-06 19:56:23 +0300

    moved all open graph tags directly in html and removed all related dynamic scripts

--- scripts/map.js
+++ scripts/map.js
@@ -1,17 +1,4 @@
 document.getElementsByTagName("title")[0].innerHTML = mapname;
-
-/*
-
-document.querySelector("[name='author']").content = "Funk";
-document.querySelector("[name='description']").content = "Map download page for the codjumper map " + mapname;
-document.querySelector("[name='keywords']").content = "codjumper, cod4, call, of, duty, 4, cj, custom, map, funk, " + mapname;
-
-document.querySelector("[property='og:title']").content = mapname + " Download";
-document.querySelector("[property='og:url']").content = url + "/" + mapname;
-document.querySelector("[property='og:image']").content = document.getElementsByTagName("map-image")[0].getElementsByTagName("a")[0].href;
-
-*/
-
 document.getElementById("header").innerHTML = mapname;
 
 document.getElementById('youtube').href = youtube;
