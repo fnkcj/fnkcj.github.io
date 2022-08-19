3bc4a2a163bd8cc749f56eda1ea49b0b7ba84c28
fnkcj <fnkattack@gmail.com>
2022-08-19 15:46:57 +0300

    moved changelogs under a dedicated button, changed sidebar buttons to actual buttons instead of being divs

--- scripts/map.js
+++ scripts/map.js
@@ -1,11 +1,23 @@
 document.getElementById("header").innerHTML = mapname;
 
+const changelogs_button = document.getElementsByClassName("changelogs")[0];
+const changelogs_content = document.getElementsByClassName("logs")[0];
+
 document.getElementById('youtube').href = youtube;
 document.getElementById('steam').href = steam;
 document.getElementById('discord').href = discord;
 
 document.getElementById(mapname).classList.add("active");
 document.getElementById("mapstyles").href = "css/" + mapname + ".css";
+
+changelogs_button.addEventListener("click", function() {
+    this.classList.toggle("active");
+    if (changelogs_content.style.maxHeight){
+    changelogs_content.style.maxHeight = null;
+    } else {
+    changelogs_content.style.maxHeight = changelogs_content.scrollHeight + "px";
+    } 
+});
 
 function changePage(page) {
     let path = window.location.pathname;
