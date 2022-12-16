d0da98307e1949c3e7f61b9f74918041eb38196b
fnkcj <fnkattack@gmail.com>
2022-12-16 11:27:18 +0200

    mapname-variable is now dynamically searched from the template title

--- scripts/global.js
+++ scripts/global.js
@@ -1,5 +1,5 @@
 const url = "https://fnkcj.github.io";
-
+const mapname = document.getElementsByTagName("title")[0].textContent;
 const youtube = "https://www.youtube.com/channel/UCeCnHuZPSwZIM8TLI2OFUlQ";
 const steam = "https://steamcommunity.com/id/funkcj";
 const discord = "https://discord.gg/NrCR2YA";
