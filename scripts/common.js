ca9b6a27d598cba3b9a76f0d5dff8d5e271d9ac7
fnkcj <fnkattack@gmail.com>
2023-03-11 16:27:32 +0200

    Fixed a bug with paths.

--- scripts/common.js
+++ scripts/common.js
@@ -3,7 +3,7 @@ document.getElementById('steam').href = steam;
 document.getElementById('discord').href = discord;
 
 function changePage(page) {
-    if (!path.includes(url)) {
+    if (path.includes(url)) {
         if (page === "index") {
             location.href = url;
         } else {
